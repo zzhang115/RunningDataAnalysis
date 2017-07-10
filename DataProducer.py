0782ec3dac175ef718d5015735836f08a6aafde9
zzhang115 <zzhang115@dons.usfca.edu>
2017-07-09 23:24:03 -0700

    some problem in send price to kafka

--- DataProducer.py
+++ DataProducer.py
@@ -3,18 +3,39 @@
 
 from kafka import KafkaProducer
 from googlefinance import getQuotes
+from kafka.errors import KafkaError, KafkaTimeoutError
+
 import argparse
 import json
+import time
+import logging
+import schedule
+
 # default kafka setting
-topic_name = 'stock_analysis'
-kafka_broker = '127.0.0.1:9002'
+# topic_name = 'stock-analysis'
+# kafka_broker = '127.0.0.1:9092'
+
+logger_format = '%(asctime)-15s %(message)s'
+logging.basicConfig(format=logger_format)
+logger = logging.getLogger('data-producer')
+
+# set logger level: TRACE, INFO(give you some information), DEBUG, WARNING, ERROR
+logger.setLevel(logging.DEBUG)
 
 def fetch_price(producer, symbol):
     #@param producer - instance of a kafka producer
     #@param symbol - symbol of the stock, string type
     #@return None
-    price = json.dumps(getQuotes(symbol))
-
+    logger.debug('Start to fetch stock price for %s', symbol)
+    try:
+        price = json.dumps(getQuotes(symbol))
+        logger.debug('Get stock price %s', price)
+        producer.send(topic = topic_name, value = price, timestamp_ms = time.time())
+        logger.debug('Sent stock price for %s to kafka', symbol)
+    except KafkaTimeoutError as timeout_error:
+        logger.warn('Failed to send stock price for %s to kafka, caused by %s', (symbol, timeout_error))
+    except Exception:
+        logger.warn('Failed to fetch stock price for %s', symbol)
 
 if __name__ == '__main__':
     # setup commandline arguments
@@ -29,8 +50,16 @@ if __name__ == '__main__':
     topic_name = args.topic_name
     kafka_broker = args.kafka_broker
 
-    # initiate a kafka producer
+    # instantiate a kafka producer
     producer = KafkaProducer(bootstrap_servers = kafka_broker)
+    fetch_price(producer, symbol)
+
+    # schedule to run every 1 second
+    # schedule.every(1).second.do(fetch_price, producer, symbol)
+    #
+    # while True:
+    #     schedule.run_pending()
+    #     time.sleep(1)
 
 
 
