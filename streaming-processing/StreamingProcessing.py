f185fe94963df1f920fff4be51ec89985113f1a2
zzhang115 <zzhang115@dons.usfca.edu>
2017-07-14 19:34:35 -0700

    add spark code

--- streaming-processing/StreamingProcessing.py
+++ streaming-processing/StreamingProcessing.py
@@ -2,14 +2,56 @@
 # 2, write data back to kafka, kafka broker, kafka topic
 
 import sys
+import atexit
+import logging
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+from kafka import KafkaProducer
+from kafka.errors import KafkaError, KafkaTimeoutError
 
 topic = ''
 new_topic = ''
 kafka_borker = ''
+logger_format = '%(asctime)-15s %(message)s'
+logging.basicConfig(format=logger_format)
+logger = logging.getLogger('stream-process')
+logger.setLevel(logging.INFO)
+
+def process(timeobj, rdd):
+    pass
+
+def shutdown_hook(producer):
+    try:
+        logger.info('flush pending messages to kafka')
+        producer.flush(10)
+        logger.info('finish flushing pending message')
+    except KafkaError as kafka_error:
+        logger.warn('Failed to flush pending message to kafka')
+
+    producer.close(10)
 if __name__ == '__main__':
     if(len(sys.argv) != 4):
         print('Usage: streaming processing [topic] [new_topic] [kafka_broker')
         exit(1)
 
+    topic, new_topic, kafka_borker = sys.argv[1:]
+    # - setup connection to spark cluster
+    # - 2 means how many cores we use for computation
+    # - spark program name
+    sc = SparkContext('local[2]', 'StockAveragePrice')
+    # - spark has its own logger
+    sc.setLogLevel('ERROR')
+    # - similar to water tap, open water tap per 5 seconds to handle data
+    ssc = StreamingContext(sc, 5)
+
+    # - create a data stream from spark
+    directKafkaStream = KafkaUtils.createDirectStream(ssc, [topic],{'metadata.broker.list' : kafka_borker})
+    # - for each RDD, do something
+    directKafkaStream.foreachRDD(process)
+    # - instantiate kafka producer
+    kafka_producer = KafkaProducer(bootstrap_servers=kafka_borker)
+    # - setup proper shutdown hook
+    atexit.register(shutdown_hook, kafka_producer)
 
 
