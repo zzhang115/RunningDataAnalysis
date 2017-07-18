c13a69333f2554d929a662f8ad822997f14daabe
zzhang115 <zzhang115@dons.usfca.edu>
2017-07-18 00:06:57 -0700

    connect to redis server refused

--- Redis/RedisPublisher.py
+++ Redis/RedisPublisher.py
@@ -3,12 +3,14 @@
 from kafka import KafkaConsumer
 import argparse
 import atexit
-import logging
 import redis
 import logging
 
-topic_name = ''
+topic_name = 'average-stock-analyzer'
 kafka_broker = '192.168.99.100:9092'
+redis_channel = 'redis-stock-analyzer'
+redis_host = '192.168.99.100'
+redis_port = '6379'
 
 logger_format = '%(asctime)-15s %(message)s'
 logging.basicConfig(format=logger_format)
@@ -24,19 +26,19 @@ def shutdown_hook():
 
 if __name__ == '__main__':
     # - setup arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('topic_name', help='the kafka topic to read from')
-    parser.add_argument('kafka_broker', help='the location of kafka broker')
-    parser.add_argument('redis_channel', help='the redis channel to publish to')
-    parser.add_argument('redis_host', help='redis server ip')
-    parser.add_argument('redis_port', help='redis server port')
-
-    args = parser.parse_args()
-    topic_name = args.topic_name
-    kafka_broker = args.kafka_broker
-    redis_channel = args.redis_channel
-    redis_host = args.redis_host
-    redis_port = args.redis_port
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('topic_name', help='the kafka topic to read from')
+    # parser.add_argument('kafka_broker', help='the location of kafka broker')
+    # parser.add_argument('redis_channel', help='the redis channel to publish to')
+    # parser.add_argument('redis_host', help='redis server ip')
+    # parser.add_argument('redis_port', help='redis server port')
+    #
+    # args = parser.parse_args()
+    # topic_name = args.topic_name
+    # kafka_broker = args.kafka_broker
+    # redis_channel = args.redis_channel
+    # redis_host = args.redis_host
+    # redis_port = args.redis_port
 
     # - setup kafka consumer
     kafka_consumer = KafkaConsumer(topic_name, bootstrap_servers=kafka_broker)
