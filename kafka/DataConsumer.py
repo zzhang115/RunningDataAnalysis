6c1260fd9f3bd5ee6e82302f7d816eec859f524d
zzhang115 <zzhang115@dons.usfca.edu>
2017-07-17 23:09:21 -0700

    add new codes

--- kafka/DataConsumer.py
+++ kafka/DataConsumer.py
@@ -1,5 +1,7 @@
 from kafka import KafkaConsumer
 
-consumer=KafkaConsumer('stock-analyzer', bootstrap_servers='192.168.99.100:9092')
+topic = 'stock-analyzer'
+new_topic = 'average-stock-analyzer'
+consumer=KafkaConsumer(new_topic, bootstrap_servers='192.168.99.100:9092')
 for msg in consumer:
     print(msg)
