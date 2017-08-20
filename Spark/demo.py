0476de69528fe0c9259fccd1ab9e0ad2a2b0c05a
zzhang115 <zzhang115@dons.usfca.edu>
2017-08-20 11:47:30 -0700

    add flatmap

--- Spark/demo.py
+++ Spark/demo.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import shutil
 from pyspark import SparkContext
 
 if __name__ == '__main__':
@@ -12,7 +14,23 @@ if __name__ == '__main__':
     # dataFilter2 = data.flatMap(lambda line: line.split(" "))
     # dataFilter2.saveAsTextFile("demo3output")
 
-    dataFilter3 = data.flatMap(lambda line: line.split(" ")).distinct()
-    dataFilter3.saveAsTextFile("demo4output")
+    # print(data) # PythonRDD[2] at RDD at PythonRDD.scala:48
+    # dataFilter3 = data.flatMap(lambda line: line.split(" ")).distinct() # flatMap: Return a new RDD by first applying a function to all elements of this RDD, and then flattening the results.
+    # dataFilter3.saveAsTextFile("demo4output")
+
+    file2 = "demo.txt"
+    if not os.path.isfile(file2):
+        print("demo file is not exits!")
+
+    # os.path.exists('demo.txt') or os.path.exits('demo5output') check if path or file exits
+    # os.remove(file) # remove file
+    # shutil.rmtree(outputDir) # remove directory
+    output = "demo5output"
+    if os.path.isdir(output):
+        print("output directory has already exits!")
+        shutil.rmtree(output)
+
+    data2 = sc.textFile(file2).flatMap(lambda line: line.split(" "))
+    data2.saveAsTextFile(output)
     sc.stop()
 
