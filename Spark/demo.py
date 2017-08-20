3e8ed530aedc6163bbc2268f7acd7a9b89a3064d
zzhang115 <zzhang115@dons.usfca.edu>
2017-08-19 22:51:54 -0700

    add spark

--- Spark/demo.py
+++ Spark/demo.py
@@ -6,7 +6,13 @@ if __name__ == '__main__':
     sc = SparkContext(appName="demo")
     data = sc.textFile(file).map(lambda line: line.upper())
     # data.saveAsTextFile("demo1output")
-    dataFilter = data.filter(lambda line : line.startswith("HI"))
-    dataFilter.saveAsTextFile("demo2output")
+    # dataFilter = data.filter(lambda line : line.startswith("HI"))
+    # dataFilter.saveAsTextFile("demo2output")
+
+    # dataFilter2 = data.flatMap(lambda line: line.split(" "))
+    # dataFilter2.saveAsTextFile("demo3output")
+
+    dataFilter3 = data.flatMap(lambda line: line.split(" ")).distinct()
+    dataFilter3.saveAsTextFile("demo4output")
     sc.stop()
 
